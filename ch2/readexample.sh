d444bd49dd2a9c5a6cf728359cbcdc2a8919b700
Qiyuan Pu <pqy7172@gmail.com>
2019-01-02 21:33:58 +0800

    add comment

--- ch2/readexample.sh
+++ ch2/readexample.sh
@@ -3,7 +3,7 @@
 echo -n "Enter your name:"
 read user_name
 
-if [ -n "$user_name" ]; then
+if [ -n "$user_name" ]; then #-n evaluates to true if its string argument is not null
 		echo "Hello $user_name!"
 		exit 0
 else
